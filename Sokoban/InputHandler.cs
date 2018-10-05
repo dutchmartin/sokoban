d065d4d2017500ade7fba3de899306db222ff0b4
Martijn Groeneveldt <martijn.groeneveldt@gmail.com>
2018-10-05 13:37:07 +0200

    inputjes

---
+++ Sokoban/InputHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sokoban
+{
+    public class InputHandler
+    {
+        public void GetArrowkeyInput(out ConsoleKey key)
+        {
+            ConsoleKey input;
+            do
+            {
+                input = Console.ReadKey().Key;
+            }
+            while ( ! IsArrowkey(input));
+            key = input;
+        }
+        private Boolean IsArrowkey(ConsoleKey key)
+        {
+            var KeyCode = (int) key;
+            var min = 37;
+            var max = 40;
+            return TestRange(KeyCode, min, max);
+        }
+        private bool TestRange(int numberToCheck, int bottom, int top)
+        {
+            return (numberToCheck >= bottom && numberToCheck <= top);
+        }
+    }
+}
