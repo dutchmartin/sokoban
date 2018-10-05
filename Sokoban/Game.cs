aca8f2b911136508a71a9f14bdecdc869bbd1605
sheertum <simonvanheertum@gmail.com>
2018-10-05 14:48:20 +0200

    implemented do a move in game model.

--- Sokoban/Game.cs
+++ Sokoban/Game.cs
@@ -8,16 +8,41 @@ namespace Sokoban
     public class Game
     {
         public MatrixItem[] Destinations { get; set; }
-        public Player Player { get; set; }
+        public Player player { get; set; }
         public DoublyLinkedMatrix Board { get; set; }
 
         public Game(DoublyLinkedMatrix board)
         {
             Board = board;
+            getDestinations();
+            player = new Player();
         }
-        public void DoMove()
+        public void DoMove(Directions direct)
         {
-            throw new System.NotImplementedException();
+            player.Move(direct);
+        }
+
+        private void getDestinations()
+        {
+            int i = 0;
+            MatrixItem leftHelper = Board.Origin;
+            MatrixItem currentElement = leftHelper;
+            while (currentElement != null)
+            {
+                
+                do
+                {
+                    if (currentElement.isDestination)
+                    {
+                        Destinations[i] = currentElement;
+                        i++;
+                    }
+
+                    currentElement = currentElement.East;
+                } while (currentElement != null);
+                leftHelper = leftHelper.South;
+                currentElement = leftHelper;
+            }
         }
     }
 }
