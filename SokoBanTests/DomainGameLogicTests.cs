c84490bb6c5dc06f50ba79f2728f44d0d96c1bd4
sheertum <simonvanheertum@gmail.com>
2018-10-07 13:18:32 +0200

    Testing toegevoegd

---
+++ SokoBanTests/DomainGameLogicTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sokoban;
+using NUnit.Framework;
+
+namespace SokoBanTests
+{
+    [TestClass]
+    public class DomainGameLogicTests
+    {
+        private Player player;
+        private Box box1;
+        private Box box2;
+
+        MatrixItem hok1;
+        MatrixItem hok2;
+        MatrixItem hok3;
+
+        #region Setup and tear down
+        [SetUp]
+        private void CreateGamestate(){
+            player = new Player();
+            box1 = new Box();
+            box2 = new Box();
+            hok1 = new MatrixItem();
+            hok2 = new MatrixItem();
+            hok3 = new MatrixItem();
+        }
+        #endregion
+
+        #region Checking moveablility
+        [TestMethod]
+        public void CanMovePlayer()
+        {
+            player.location = hok1;
+
+        }
+        #endregion
+    }
+}
